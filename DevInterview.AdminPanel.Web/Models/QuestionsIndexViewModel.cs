858257e9f4033b4526f9fe6f47430548d0a845dc
Javier Guerrero <javier@bertonisolutions.com>
2024-02-22 18:45:06 -0500

    add question modal

--- DevInterview.AdminPanel.Web/Models/QuestionsIndexViewModel.cs
+++ DevInterview.AdminPanel.Web/Models/QuestionsIndexViewModel.cs
@@ -5,6 +5,7 @@
         public string SelectedRoleId { get; set; }
         public string SelectedTopicId { get; set; }
         public List<RoleViewModel> RoleList { get; set; } = new();
+        public List<TopicViewModel> TopicList { get; set; } = new();
         public List<QuestionViewModel> QuestionList { get; set; } = new();
     }
 }
